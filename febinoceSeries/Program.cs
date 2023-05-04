a083e09002d9c388ce2aea52420da4be27ee96ae
Rahulalvk <rahulal432@gmail.com>
2023-05-03 21:59:00 -0700

    Febinacci series program

---
+++ febinoceSeries/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace febinoceSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Febinoci.DisplayFebinoceSeries();
+        }
+    }
+}
